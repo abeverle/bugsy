52695d48513fef806940dcd69726efc85c353572
Kimberly <klindholm7@gmail.com>
2020-10-29 12:59:07 -0600

    removed unused stuff

--- computer/contorl.py
+++ computer/contorl.py
@@ -6,47 +6,6 @@ import numpy as np
 
 ard = serial.Serial("COM4", 9600)
 
-fourPoint = [
-    (1, 0, 0.5),
-    (1, 1, 0.1),
-    (1, -1, 0.1),
-    (1, 1, 2),
-    (-1, -1, 2),
-    (1, 1, 2),
-    (-1, -1, 2)
-]
-
-motionTest = []
-
-for w in [-1, 0, 1]:
-    for v in [-1, 1]:
-        for t in np.linspace(1, 2, num=5):
-            for _ in range(0, 2):
-                motionTest.append((v, w, t))
-                motionTest.append((0, 0, 10))
-
-# cmd in the form of (v, w, t) 
-# -1 <= v, w, <= 1
-# t is sec
-def runCmds(cmds):
-    for cmd in cmds:
-        v, w, t = cmd
-        ctrl_v = int(abs(v) * 255)
-        ctrl_w = int(abs(w) * 255)
-        ctrl_d = 0
-        if v > 0:
-            ctrl_d += 1
-        if w < 0:
-            ctrl_d += 2
-
-        print(cmd)        
-        cmdstr = bytes([ctrl_v, ctrl_w, ctrl_d])
-        for _ in range(int(t*100)):
-            ard.write(cmdstr)
-            time.sleep(0.01)
-
-# runCmds(fourPoint)
-
 sensorArr = [0,0]
 
 while True:
